31578dc9096f2d3c0b16367990764098ed2cc7f7
Aidan Kirvan <apkirvan@hotmail.com>
2021-09-02 12:33:28 +1000

    win and loss functionality

--- src/data/initialState.js
+++ src/data/initialState.js
@@ -1,14 +1,15 @@
 const initialState = {
-    gameStart: false,
-    gameVictory: null,
-    gameLoss: null,
-    userTurn: null,
-    monster: null,
+    gameStart: false, // becomes true at game start
+    gameVictory: null, //set to win or lose
+    userTurn: null, // alternates between true and false
+    monster: null, // stores monster object
     monsterMaxHealth: null,
     monsterCurrentHealth: null,
-    playerCurrentHealth: null,
-    playerMaxHealth: null,
+    playerName: "Nicholas Cage",
+    playerCurrentHealth: 0,
+    playerMaxHealth: 0,
     playerDamage: 6,
+    playerArmour: 10,
     gameRound: 0,
     damageReport: ''
 }
--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -11,21 +11,36 @@ const gameReducer = (state, action) => {
                 ...state,
                 "monster": action.data,
                 "monsterCurrentHealth": action.data.health,
-                "monsterMaxHealth": action.data.health
+                "monsterMaxHealth": action.data.health,
+                "userTurn": true, // it's the users turn every time a monster gets rolled
+                "gameRound": state.gameRound + 1
             }
 
         case 'setMonsterCurrentHealth':
             return {
                 ...state,
-                "monsterCurrentHealth": action.data
+                "monsterCurrentHealth": state.monsterCurrentHealth - action.data,
+                "userTurn": false
             }
 
-        case 'setStartGame':
+        case 'setPlayerCurrentHealth':
             return {
                 ...state,
-                "gameStart": true
+                "playerCurrentHealth": state.playerCurrentHealth - action.data,
+                "userTurn": true
             }
 
+        case 'setGameStart':
+            return {
+                ...state,
+                "gameStart": true,
+                "playerCurrentHealth": 40,
+                "playerMaxHealth": 40,
+                "damageReport": "What do you do?"
+            }
+
+
+        //DEFAULT
         default: {
             return null
         }
